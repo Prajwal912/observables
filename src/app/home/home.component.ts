165d9d2b158a704907accd3b3701ed7867008bdc
prajwal <prajwalshah912@gmail.com>
2023-07-23 03:15:40 +0530

    observables error

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,16 +16,22 @@ export class HomeComponent implements OnInit,OnDestroy {
     // this.firstObsSubs = interval(1000).subscribe((count) => console.log(count))
 
     const customObs = Observable.create(observer => {
-        let count = 0
+        let count = 1
         setInterval(() => {
           observer.next(count);
+          if(count> 4){
+            observer.error(new Error ('count is exceeds to 5'))
+          }
           count++
         },1000)
     })
 
     this.firstObsSubs = customObs.subscribe(data => {
       console.log(data)
-    })
+    }, error => {
+      console.log(error)
+    }
+    )
   }
 
   ngOnDestroy(): void {
